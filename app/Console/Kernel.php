e33ee955f6975377c9574b98ca10593de287a134
Isaiah Proctor <proctoi@onecollective.org>
2024-01-27 03:27:34 +0000

    removed every minuent remove reals

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -12,7 +12,6 @@ class Kernel extends ConsoleKernel {
     protected function schedule(Schedule $schedule): void {
         // $schedule->command('inspire')->hourly();
         $schedule->command('app:clear-book-reals')->dailyAt('00:00');
-        $schedule->command('app:clear-book-reals')->everyMinute();
 
     }
 
